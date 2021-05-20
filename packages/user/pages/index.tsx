7a07f54e67c4329a210e7bf28ab844f1482c773c
niteshwosti <niteshwosti13@gmail.com>
2021-05-20 18:47:00 +0545

    Linaria Demo with Css and Styled Modules

--- packages/user/pages/index.tsx
+++ packages/user/pages/index.tsx
@@ -1,7 +1,23 @@
+import { css } from "linaria/";
+import { styled } from "linaria/react";
+
+const Heading = styled.h1`
+  color: red;
+  font-family: "Courier New", Courier, monospace;
+`;
+
+const paragraph = css`
+  color: blue;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+`;
 export default function Home() {
   return (
     <>
-      <h1>Hello from Mono Repo</h1>
+      <Heading>Hello from styled Linaria Heading. </Heading>
+      <p className={paragraph}>
+        This is a paragraph from css module of linaria
+      </p>
     </>
   );
 }
